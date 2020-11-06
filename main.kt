d31820f57ffeb78afc674125487652c48ac1fcf3
Zachtfox <31944870+Zachtfox@users.noreply.github.com>
2020-11-06 03:17:12 -0600

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var count = 0
+    var MyArray = arrayOf<String>("Ice Cream", "Ice cream Scoop", "Ice Cream Cone", "Spoons", "Napkins")
+     for (count in MyArray){
+         println(count)
+     }
+}
